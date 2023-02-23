680df30923bf25bd973d11a39d4e55ff038ad91a
bascioTOja <hjhjg55sajda@interia.pl>
2023-02-23 22:03:51 +0100

    Move events to game class

--- classes/game.py
+++ classes/game.py
@@ -4,6 +4,8 @@ from dataclasses import dataclass
 from classes import helpers
 from classes.track_controller import TrackController
 from classes.train_controller import TrainController
+from enums.train_type import TrainType
+
 
 @dataclass
 class Game:
@@ -18,12 +20,36 @@ class Game:
         self.win = pygame.display.set_mode(self.get_size())
         self.clock = pygame.time.Clock()
 
-        self.track_controller = TrackController()
-
-
         tracks, trains = helpers.create_tracks_and_trains()
+        self.track_controller = TrackController()
         self.track_controller.tracks.extend(tracks)
         self.train_controller = TrainController(trains)
+
+    def events(self):
+        self.pos = pygame.mouse.get_pos()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit()
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                self.mouse_button_up_events(event)
+
+            if event.type == pygame.KEYDOWN:
+                self.keydown_events(event)
+
+            # elif event.type == pygame.MOUSEBUTTONUP:
+            #     if event.button == 1:
+            #         selected_point = None
+            #         spline.positions = spline.evaluate_spline(spline.t)
+            #
+            # elif event.type == pygame.MOUSEMOTION:
+            #     cursor_position = pygame.mouse.get_pos()
+            #     if selected_point:
+            #         pos = pygame.mouse.get_pos()
+            #         spline.positions = spline.evaluate_spline(spline.t)
+            #         selected_point.x, selected_point.y = pos
+            #         selected_point.rect.x, selected_point.rect.y = selected_point.x - selected_point.size // 2, selected_point.y - selected_point.size // 2
 
     def draw(self, *args) -> None:
         self.win.fill((0, 0, 0))
@@ -41,3 +67,22 @@ class Game:
 
     def get_size(self) -> tuple[int, int]:
         return self.width, self.height
+
+    def quit(self):
+        self.run = False
+
+    def mouse_button_up_events(self, event):
+        if event.button == 1:
+            self.track_controller.add_node(self.pos)
+        elif event.button == 3:
+            self.track_controller.remove_node(self.pos)
+
+    def keydown_events(self, event):
+        if event.key == pygame.K_SPACE:
+            self.track_controller.create_track()
+        if event.key == pygame.K_q:
+            self.train_controller.remove_train(self.pos)
+        if event.key == pygame.K_e:
+            if (selected_track := self.track_controller.get_first_track_in_position(self.pos)) is not None:
+                self.train_controller.add_train(TrainType.CLASSIC, selected_track, self.pos)
+
--- main.py
+++ main.py
@@ -2,45 +2,11 @@ import pygame
 
 from classes.game import Game
 
-from enums.train_type import TrainType
-
-
 def main():
     game = Game(size=(800, 600), fps=60)
 
     while game.run:
-        game.pos = pygame.mouse.get_pos()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                game.run = False
-            if event.type == pygame.MOUSEBUTTONUP:
-                if event.button == 1:
-                    game.track_controller.add_node(game.pos)
-                elif event.button == 3:
-                    game.track_controller.remove_node(game.pos)
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    game.track_controller.create_track()
-                if event.key == pygame.K_q:
-                    game.train_controller.remove_train(game.pos)
-                if event.key == pygame.K_e:
-                    if (selected_track := game.track_controller.get_first_track_in_position(game.pos)) is not None:
-                        game.train_controller.add_train(TrainType.CLASSIC, selected_track, game.pos)
-            # elif event.type == pygame.MOUSEBUTTONUP:
-            #     if event.button == 1:
-            #         selected_point = None
-            #         spline.positions = spline.evaluate_spline(spline.t)
-            #
-            # elif event.type == pygame.MOUSEMOTION:
-            #     cursor_position = pygame.mouse.get_pos()
-            #     if selected_point:
-            #         pos = pygame.mouse.get_pos()
-            #         spline.positions = spline.evaluate_spline(spline.t)
-            #         selected_point.x, selected_point.y = pos
-            #         selected_point.rect.x, selected_point.rect.y = selected_point.x - selected_point.size // 2, selected_point.y - selected_point.size // 2
-
+        game.events()
         game.update(game.train_controller)
         game.hover(game.train_controller, game.track_controller)
         game.draw(game.track_controller, game.train_controller)
